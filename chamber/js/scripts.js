dfaf642890aa24ee101cd2fd3787e1519927b10c
lg10migue <lop18028@byui.edu>
2022-05-13 13:45:44 -0600

    Added chamber home page.
    Added styles, scripts and html for Chamber Home Page.

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,5 +1,48 @@
+// Dates
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+] ;
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+] ;
+
 let currentDate = new Date() ;
 let lastModification = document.lastModified ;
+let day = currentDate.getDay() ;
+let dayName = days[ day ] ;
+let month = months[ currentDate.getMonth() ] ;
+let year = currentDate.getFullYear() ;
 
 document.getElementById( "lastupdate" ).textContent = lastModification ;
-document.querySelector( "span" ).textContent = currentDate.getFullYear() ;
+document.getElementById( "year" ).textContent = year ;
+document.getElementById( "date" ).textContent = `${dayName}, ${day} ${month} ${year}`
+
+// Dynamic Menu
+
+function toggleMenu() {
+    document.getElementById( "primaryNav" ).classList.toggle( "open" )
+    document.getElementById( "hamburgerBtn" ).classList.toggle( "open" )
+}
+
+const x = document.getElementById( "hamburgerBtn" )
+x.onclick = toggleMenu ;
