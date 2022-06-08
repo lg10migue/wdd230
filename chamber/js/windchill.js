647b62f06e28efe67115a985dd9835de46f48df6
lg10migue <lop18028@byui.edu>
2022-06-07 20:19:20 -0600

    Fixed images fo Chamber Home Page.
    Fixed the display for the images in Chamber Home Page, also add an else statement for the windchill JS file.

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,7 @@
 // Get values from page.
 let temperature = parseFloat( document.getElementById( "temperature" ).innerHTML ) ;
 let windSpeed = parseFloat( document.getElementById( "wind-speed" ).innerHTML ) ;
+let windChillElement = document.getElementById( "wind-chill" ) ;
 
 // Transform temperature to Fahrenheit.
 let temperatureToFahrenheit = ( temperature * 1.8 ) + 32 ;
@@ -11,6 +12,7 @@ let windSpeedToMPH = windSpeed / 1.609 ;
 if ( ( temperatureToFahrenheit <= 50 ) && ( windSpeedToMPH > 3 ) ) {
     windChill = 35.74 + ( 0.6215 * temperatureToFahrenheit ) - ( 35.75 * ( windSpeedToMPH ** 0.16 ) ) + ( 0.4275 * temperatureToFahrenheit * ( windSpeedToMPH ** 0.16 ) ) ;
     windChillToCelsius = ( windChill - 32 ) / 1.8 ;
-    let windChillElement = document.getElementById( "wind-chill" ) ;
     windChillElement.innerHTML = windChillToCelsius.toFixed( 2 ) ;
+} else {
+    windChillElement.innerHTML = "N/A" ;
 } ;
