9aea74272def804a98501dd65bdb8abed85c3a57
lg10migue <lop18028@byui.edu>
2022-07-03 01:12:23 -0600

    Change image in hero div.
    Adjust image for hero div.

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -4,13 +4,14 @@ const images = document.querySelectorAll( "[data-src]" ) ;
 // Pre load images.
 function preloadImage( img ) {
 	const scr = img.getAttribute( "data-src" ) ;
+    img.removeAttribute( "data-src" ) ;
 	img.src = scr ;
 }
 
 // Options for imageObserver.
 const imageOptions = {
 	threshold: 1,
-	rootMargin: "0px 0px -150px 0px",
+	rootMargin: "0px 0px 430px 0px",
 } ;
 
 // Function observer.
