44e61eeaa296660e4409bfaef745647ef7797c82
lg10migue <lop18028@byui.edu>
2022-07-22 11:22:56 -0600

    Last details.
    Added last details.

--- temple-inn/js/weather_by_temple.js
+++ temple-inn/js/weather_by_temple.js
@@ -90,7 +90,7 @@ async function apiFetch() {
         const response = await fetch( url ) ;
         if ( response.ok ) {
             const data = await response.json() ;
-            // console.log( data ) ; // this is for testing the call
+            console.log( data ) ; // this is for testing the call
 
             // Current weather.
             displayResults( data ) ;
